b6656267f263f894de71dcf2094b35fd8c171520
Janjan <bubble-gumz@hotmail.com>
2017-10-14 11:52:16 +0700

    OK

---
+++ Hello Swift4/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Hello Swift4
+//
+//  Created by Janjan on 10/14/2560 BE.
+//  Copyright © 2560 Janjanx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Explicit การประกาศตัวแปร แบบกำหนดค่า
+    var nameString: String = " "
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    } // Main function สิ่งแรกที่จะทำงาน
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+} // Main Class
+
